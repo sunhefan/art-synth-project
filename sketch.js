c75b69c53b9b86b18452dfc0d5c3958b4917370c
HeFan Lim <hefanlim@HeFans-MBP.hsd1.ca.comcast.net>
2019-11-14 13:38:51 -0800

    changed

--- sketch.js
+++ sketch.js
@@ -1,36 +1,39 @@
+//all code highly based off of Dan Shiffman's processing tutorials https://www.youtube.com/watch?v=IKB1hWWedMk
+
+//SoundFile file;
 var fft;
 let fft2;
-
+var mic;
 var bands = 1024; // resolution of the FFT
 var spectrum, spectrum2;
 var terrain = [];
 var terrain2 = [];
-let zoom = 20;
+let zoom = 2;
 let peak = 80;
 //let notes = ["A7","F6","G6","E6","D6"," "," "," "," "," "," "];
 let notes = [
-  ["A5", "C#5", "E5"],
-  ["A5", "C5", "E5"],
+  ["A7", "F6", "G6"],
+  ["E6", "D6", "E5"],
   ["D5", "F#5", "A5"],
   ["G4", "B5", "D5"],
-  ["E4", "G4", "B4"],
-  ["C3", "E3", "G3"],
-  ["F3", "A3", "C4"]
+  ["E6", "G4", "B6"],
+  ["C7", "D7", "E7"],
+  ["F7", "G7", "A5"]
 ];
 
 let keynotes = [
-  ["C2", "E2", "G2"],
-  ["F2", "A2", "C2"],
-  ["A2", "C2", "E2"],
-  ["B2", "D2", "F#2"],
+  ["C4", "D4", "E4"],
+  ["F3", "G3", "A3"],
+  ["A4", "C4", "E4"],
+  ["B2", "D3", "F#4"],
   ["C3", "E3", "G3"],
   ["F3", "A3", "C3"],
   ["A3", "C3", "E3"],
-  ["B3", "D3", "F#3"]
+  ["B3", "D3", "F#2"]
 ];
 
 
-var lines = 3;
+var lines = 1;
 let duration;
 let durationk;
 let trigger;
@@ -40,73 +43,78 @@ let px = 0;
 let notecounter = 10;
 
 function setup() {
-  createCanvas(800, 800);
-  //frameRate(15);
-  //file = new SoundFile(this, "03badnews.mp3");
+  createCanvas(windowWidth, windowHeight);
 
-  // file = new SoundFile(this, "popcorn.mp3");
-  //terrain = new float [lines][bands];
-  // create fft object
-  fft = new p5.FFT(0.8, bands);
-  fft2 = new p5.FFT(0.8, bands);
+  fft = new p5.FFT(0.85, bands);
+  fft2 = new p5.FFT(0.85, bands);
+  // create audion in
+  mic = new p5.AudioIn();
 
 
+
+
+  //// start the Audio Input
+  mic.start();
   synth = new p5.PolySynth();
   synthkey = new p5.PolySynth();
-  //  synth.AudioVoice = 16;
+   // synth.AudioVoice = 16;
 
-  // fft.setInput(synth);
+  fft.setInput(synth);
   fft2.setInput(synthkey);
 
 
-  for (let y = 0; y < lines; y++) {
+  for (let y = 0; y <=lines; y++) {
     terrain[y] = [];
-    for (let x = 0; x < bands; x++) {
+    for (let x = 0; x <= bands; x++) {
       terrain[y][x] = 0;
     }
   }
 
-  for (let y = 0; y < lines; y++) {
+  for (let y = 0; y <= lines; y++) {
     terrain2[y] = [];
-    for (let x = 0; x < bands; x++) {
+    for (let x = 0; x <= bands; x++) {
       terrain2[y][x] = 0;
     }
   }
-  //strokeWeight(2);
-  // setInterval(chooseNote, 120);
+
 }
 
 function draw() {
-  background(50 );
+  background(0 );
   fill(frameCount % 300, 100, 200);
-
-  var c = color(50);
+  translate(-width/2,height/2,50);
+  var c = color(frameCount % 300, 100, 200);
   stroke(c);
 
- zoom = map(mouseX, 0, width, 2, 100);
-
+  // translate(-width / 2, 30,0);
+  //  rotateX(radians(-60));
+ zoom = map(0,mouseY, 0, height, 70);
+  // translate(10, 10);
+  // rotateZ(radians(rotZ));
+  // -translate(0, -600);
   spectrum = fft.analyze();
   spectrum2 = fft2.analyze();
 
   for (let y = 0; y < terrain2.length; y++) {
     beginShape();
-    vertex(0, ((height / lines) * y + 70));
-    vertex(20, ((height / lines) * y + 70));
+    vertex(10, ((height / lines) * y + 70));
+    vertex(30, ((height / lines) * y + 70));
     for (let x = 0; x < spectrum2.length; x++) {
-      //  stroke(255,255,map(x,0,bands/zoom,0,255))
-      vertex(map(x, 0, bands / zoom, 20, width / 2), (map(terrain2[y][x], 0, 255, 0, -peak)) + ((height / lines) * y + 70));
+      vertex(map(x, 0, bands / zoom, 20, width*2), (map(terrain2[y][x], 0, 255, 0, -peak)) + ((height / lines) * y + 70));
     }
     vertex(0, ((height / lines) * y + 70));
     endShape();
+
+
   }
 
 
   for (let y = 0; y < terrain.length; y++) {
     beginShape();
-    // vertex(0, ((height / lines) * y + 70));
-    //vertex(10, ((height / lines) * y + 70));
+    vertex(10, ((height / lines) * y + 70));
+    vertex(10, ((height / lines) * y + 70));
     for (let x = 0; x < spectrum.length; x++) {
-      //  stroke(255,255,map(x,0,bands/zoom,0,255))
+
       vertex(map(x, 0, bands / zoom, width / 2, width), (map(terrain[y][x], 0, 255, 0, -peak)) + ((height / lines) * y + 70));
     }
     vertex(width / 2, ((height / lines) * y + 70))
@@ -114,7 +122,8 @@ function draw() {
   }
 
 
-  for (var y = lines - 1; y >= 0; y--) {
+  // for (let y = 0; y < terrain.length; y++) {
+  for (let y = lines - 1; y >= 0; y--) {
     //for (var i = 0; i < bands; i++) {
     terrain2[0] = spectrum2;
     if (y > 0) {
@@ -124,7 +133,7 @@ function draw() {
     }
   }
 
-  for (var y = lines - 1; y >= 0; y--) {
+  for (let y = lines - 1; y >= 0; y--) {
     //for (var i = 0; i < bands; i++) {
     terrain[0] = spectrum;
     if (y > 0) {
@@ -153,10 +162,14 @@ function mouseReleased() {
 
 function chooseNote() {
   // synth.noteRelease();
-  let chordPicker =  floor(map(mouseX, 10, width, 0, notes.length));
-
+  let chordPicker = floor(random(notes.length));
+  // console.log(chordPicker);
+  duration = random(0.2, 1.0);
+  //  play(note, [velocity], [secondsFromNow], [sustainTime])
+  //synth.play(notes[floor(random(notes.length))], 0.5, .15, .1);
+  //synth.triggerAttack(notes[floor(random(notes.length))], 0.3, 0, duration);
   for (let i = 0; i < 3; i++) {
-    synth.noteAttack(notes[chordPicker][i], i*0.8 , 0, duration);
+    synth.noteAttack(notes[chordPicker][i], i * 0.8, 0, duration);
   }
 }
 
@@ -169,6 +182,6 @@ function chooseKeyNote() {
   //synth.play(notes[floor(random(notes.length))], 0.5, .15, .1);
   //synth.triggerAttack(notes[floor(random(notes.length))], 0.3, 0, duration);
   for (let i = 0; i < 3; i++) {
-    synthkey.noteAttack(keynotes[chordPicker2][i], i * 0.8, 0, durationk);
+    synthkey.noteAttack(keynotes[chordPicker2][i], i * 0.2, 0, durationk);
   }
 }
